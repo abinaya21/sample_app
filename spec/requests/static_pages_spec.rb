91c84b03b5a5368e687c1b2f0d1caf3dfa875b64
Abinaya <abinaya@freshdesk.com>
2016-03-28 14:37:07 +0530

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,14 +6,22 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
 
+  shared_examples_for "all static pages" do
+
+    it  { should have_selector('h1', text: heading) }
+
+    it  { should have_selector('title', text: full_title(page_title)) }
+
+  end
+
   describe "Home page" do
 
     before { visit root_path }
 
-    it  { should have_selector('h1', :text => 'Sample App') }
+    let(:heading) { "Sample App" }
+    let(:page_title) { "" }
 
-    it  { should have_selector('title', :text => full_title('')) }
-	 
+    it_should_behave_like "all static pages"
     it { should_not have_selector('title', :text => " | Home") }
 
   end
@@ -22,9 +30,10 @@ describe "StaticPages" do
 
     before { visit help_path }
 
-    it  { should have_selector('h1', :text => 'Help') }
+    let(:heading) { "Help" }
+    let(:page_title) { "Help" }
 
-    it  { should have_selector('title', :text => full_title('Help')) }
+    it_should_behave_like "all static pages"
 
   end
 
@@ -32,20 +41,37 @@ describe "StaticPages" do
 
     before { visit about_path }
 
-    it  { should have_selector('h1', :text => 'About') }
+    let(:heading) { "About" }
+    let(:page_title) { "About Us" }
 
-    it  { should have_selector('title', :text => full_title('About Us')) }
+    it_should_behave_like "all static pages"
 
   end
 
   describe "Contact" do
 
   	before { visit contact_path }
+    let(:heading) { "Contact" }
+    let(:page_title) { "Contact" }
 
-    it  { should have_selector('h1', :text => 'Contact') }
+    it_should_behave_like "all static pages"
 
-    it  { should have_selector('title', :text => full_title('Contact')) }
+  end
 
+  it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      should have_selector 'title', text: full_title('About Us')
+      click_link "Help"
+      should have_selector 'title', text: full_title('Help')
+      click_link "Contact"
+      should have_selector 'title', text: full_title('Contact')
+      click_link "Home"
+      should have_selector 'title', text: full_title('')
+      click_link "Sign up now!"
+      should have_selector 'title', text: full_title('Sign up')
+      click_link "sample app"
+      should have_selector 'title', text: full_title('')
   end
 
 end
