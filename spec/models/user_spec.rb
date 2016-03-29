7c77c415f74ef822ba7228c5902346e17d1b9a9e
Abinaya <abinaya@freshdesk.com>
2016-03-29 16:35:11 +0530

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token)}
   it { should be_valid }
 
   describe "when name is not present" do
@@ -115,5 +116,10 @@ describe User do
       @user.save
       @user.reload.email.should == uppercase_email.downcase
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
