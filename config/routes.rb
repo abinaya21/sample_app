8344471b8a8782c20a3bc5f4cc37146ed368b250
Abinaya <abinaya@freshdesk.com>
2016-04-01 15:28:11 +0530

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root to: "static_pages#home"
   
@@ -16,6 +17,12 @@ SampleApp::Application.routes.draw do
   match "/signin", to: "sessions#new"
 
   match "/signout", to: "sessions#destroy", via: :delete
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/factories/relationships.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :relationship do
+    follower_id "MyString"
+    integer "MyString"
+    followed_id "MyString"
+    integer "MyString"
+  end
+end
